5ce7d6cef267d99d327d7fb77d951e34990385e9
Abhishek Rathore <rathorabhishek872@gmail.com>
2026-03-19 19:28:27 +0530

    chore(server): make client and server files for orpc with tanstack query

---
+++ lib/orpc/server.ts
@@ -0,0 +1,18 @@
+import { headers } from "next/headers";
+
+import { router } from "@/server";
+import { createRouterClient } from "@orpc/server";
+import "server-only";
+
+globalThis.$client = createRouterClient(router, {
+  /**
+   * Provide initial context if needed.
+   *
+   * Because this client instance is shared across all requests,
+   * only include context that's safe to reuse globally.
+   * For per-request context, use middleware context or pass a function as the initial context.
+   */
+  context: async () => ({
+    headers: await headers(), // provide headers if initial context required
+  }),
+});
